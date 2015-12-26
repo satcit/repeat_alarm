546129aa5411ce6e5ddd3510b41676e25055c36a
Aleksei Timofeev <aleksei.t@scopicsoftware.com>
2015-12-26 11:57:55 +0300

    Added music, spinners, rearranged layout

--- app/src/main/java/ru/satcit/repeat_alarm/MainActivity.java
+++ app/src/main/java/ru/satcit/repeat_alarm/MainActivity.java
@@ -4,70 +4,111 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.RadioButton;
+import android.widget.Spinner;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends ActionBarActivity {
-    public static final String PREFERENCES_NAME = "alarmrepeater.preferences";
+  public static final String PREFERENCES_NAME = "repeatalarm.preferences";
 
-    private EditText repeatDurationText;
-    private EditText durationText;
-    private Button startAlarmButton;
-    private SharedPreferences preferences;
+  private EditText repeatRateText;
+  private Spinner repeatRateUnitSpinner;
+  private EditText repeatDurationText;
+  private Spinner repeatDurationUnitSpinner;
+  private EditText restDurationText;
+  private Spinner restDurationUnitSpinner;
+  private Button startAlarmButton;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
 
-        bindControls();
+    bindControls();
+    loadSettings();
+    initListeners();
+  }
 
-        preferences = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
-        repeatDurationText.setText(Integer.toString(preferences.getInt(getString(R.string.alarmRepeatDuration), getResources().getInteger(R.integer.defaultAlarmRepeatDuration))));
-        durationText.setText(Integer.toString(preferences.getInt(getString(R.string.alarmDuration), getResources().getInteger(R.integer.defaultAlarmDuration))));
+  private void bindControls() {
+    repeatRateText = (EditText)findViewById(R.id.repeatRateText);
+    repeatRateUnitSpinner = (Spinner)findViewById(R.id.repeatRateUnitSpinner);
+    repeatDurationText = (EditText)findViewById(R.id.repeatDurationText);
+    repeatDurationUnitSpinner = (Spinner)findViewById(R.id.repeatDurationUnitSpinner);
+    restDurationText = (EditText)findViewById(R.id.restDurationText);
+    restDurationUnitSpinner = (Spinner)findViewById(R.id.restDurationUnitSpinner);
+    startAlarmButton = (Button)findViewById(R.id.startAlarmButton);
 
-        initListeners();
-    }
+    ArrayAdapter<TimeUnit> timeUnitValues = new ArrayAdapter<TimeUnit>(this, android.R.layout.simple_spinner_item, TimeUnit.values());//ArrayAdapter.createFromResource(this, R.array.timeUnits, android.R.layout.simple_spinner_item);
+    timeUnitValues.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+    repeatRateUnitSpinner.setAdapter(timeUnitValues);
+    repeatDurationUnitSpinner.setAdapter(timeUnitValues);
+    restDurationUnitSpinner.setAdapter(timeUnitValues);
+  }
 
-    private void bindControls() {
-        repeatDurationText = (EditText) findViewById(R.id.alarmRepeatDurationText);
-        durationText = (EditText) findViewById(R.id.alarmDurationText);
-        startAlarmButton = (Button) findViewById(R.id.startAlarmButton);
-    }
-
-    private void initListeners() {
-        startAlarmButton.setText(isServiceRunning(RepeatService.class) ? R.string.stop : R.string.start);
-        startAlarmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(!isServiceRunning(RepeatService.class)) {
-                    RadioButton minutesRadio = (RadioButton) MainActivity.this.findViewById(R.id.alarmRepeatDurationMinutes);
-                    SharedPreferences.Editor editor = preferences.edit();
-                    int repeatDuration = Integer.parseInt(repeatDurationText.getText().toString());
-                    editor.putInt(getString(R.string.alarmRepeatDuration), minutesRadio.isChecked() ? repeatDuration * 60 : repeatDuration);
-                    editor.putInt(getString(R.string.alarmDuration), Integer.parseInt(durationText.getText().toString()));
-                    editor.apply();
-                    startService(new Intent(MainActivity.this, RepeatService.class));
-                    startAlarmButton.setText(R.string.stop);
-                } else {
-                    stopService(new Intent(MainActivity.this, RepeatService.class));
-                    startAlarmButton.setText(R.string.start);
-                }
-            }
-        });
-    }
-
-    private boolean isServiceRunning(Class<?> serviceClass) {
-        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if (serviceClass.getName().equals(service.service.getClassName())) {
-                return true;
-            }
+  private void initListeners() {
+    startAlarmButton.setText(isServiceRunning(RepeatService.class) ? R.string.stop : R.string.start);
+    startAlarmButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        if (!isServiceRunning(RepeatService.class)) {
+          saveSettings();
+          startRepeatService();
+          startAlarmButton.setText(R.string.stop);
+        } else {
+          stopService(new Intent(MainActivity.this, RepeatService.class));
+          startAlarmButton.setText(R.string.start);
         }
-        return false;
+      }
+    });
+  }
+
+  private boolean isServiceRunning(Class<?> serviceClass) {
+    ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+    for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+      if (serviceClass.getName().equals(service.service.getClassName())) {
+        return true;
+      }
     }
+    return false;
+  }
+
+  private void startRepeatService() {
+    Intent intent = new Intent(this, RepeatService.class);
+    //TODO validate parameters
+    intent.putExtra(RepeatService.AUDIO_RESID_PROPERTY, R.raw.sound);
+    intent.putExtra(RepeatService.REPEAT_DURATION_PROPERTY, Long.parseLong(repeatDurationText.getText().toString()));
+    intent.putExtra(RepeatService.REPEAT_DURATION_UNIT_PROPERTY, ((TimeUnit)repeatDurationUnitSpinner.getSelectedItem()).name());
+    intent.putExtra(RepeatService.REPEAT_RATE_PROPERTY, Long.parseLong(repeatRateText.getText().toString()));
+    intent.putExtra(RepeatService.REPEAT_RATE_UNIT_PROPERTY, ((TimeUnit)repeatRateUnitSpinner.getSelectedItem()).name());
+    intent.putExtra(RepeatService.REST_DURATION_PROPERTY, Long.parseLong(restDurationText.getText().toString()));
+    intent.putExtra(RepeatService.REST_DURATION_UNIT_PROPERTY, ((TimeUnit)restDurationUnitSpinner.getSelectedItem()).name());
+    startService(intent);
+  }
+
+  private void saveSettings() {
+    SharedPreferences.Editor editor = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE).edit();
+    editor.putLong(RepeatService.REPEAT_DURATION_PROPERTY, Long.parseLong(repeatDurationText.getText().toString()));
+    editor.putString(RepeatService.REPEAT_DURATION_UNIT_PROPERTY, ((TimeUnit) repeatDurationUnitSpinner.getSelectedItem()).name());
+    editor.putLong(RepeatService.REPEAT_RATE_PROPERTY, Long.parseLong(repeatRateText.getText().toString()));
+    editor.putString(RepeatService.REPEAT_RATE_UNIT_PROPERTY, ((TimeUnit) repeatRateUnitSpinner.getSelectedItem()).name());
+    editor.putLong(RepeatService.REST_DURATION_PROPERTY, Long.parseLong(restDurationText.getText().toString()));
+    editor.putString(RepeatService.REST_DURATION_UNIT_PROPERTY, ((TimeUnit)restDurationUnitSpinner.getSelectedItem()).name());
+    editor.apply();
+  }
+
+  private void loadSettings() {
+    SharedPreferences preferences = getSharedPreferences(PREFERENCES_NAME, MODE_PRIVATE);
+    repeatDurationText.setText(String.format("%d", preferences.getLong(RepeatService.REPEAT_DURATION_PROPERTY, 0L)));
+    repeatDurationUnitSpinner.setSelection(TimeUnit.valueOf(preferences.getString(RepeatService.REPEAT_DURATION_UNIT_PROPERTY, TimeUnit.SECONDS.name())).ordinal());
+    repeatRateText.setText(String.format("%d", preferences.getLong(RepeatService.REPEAT_RATE_PROPERTY, 0L)));
+    repeatRateUnitSpinner.setSelection(TimeUnit.valueOf(preferences.getString(RepeatService.REPEAT_RATE_UNIT_PROPERTY, TimeUnit.SECONDS.name())).ordinal());
+    restDurationText.setText(String.format("%d", preferences.getLong(RepeatService.REST_DURATION_PROPERTY, 0L)));
+    restDurationUnitSpinner.setSelection(TimeUnit.valueOf(preferences.getString(RepeatService.REST_DURATION_UNIT_PROPERTY, TimeUnit.SECONDS.name())).ordinal());
+  }
 }
--- app/src/main/java/ru/satcit/repeat_alarm/RepeatService.java
+++ app/src/main/java/ru/satcit/repeat_alarm/RepeatService.java
@@ -1,79 +1,116 @@
 package ru.satcit.repeat_alarm;
 
-import java.util.Date;
 import android.app.Service;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.os.Bundle;
 import android.os.IBinder;
-import android.os.Vibrator;
 
-/**
- * Created by Aleksei on 20.12.2015.
- */
+import java.util.Arrays;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
-public class RepeatService extends Service implements Runnable {
-    private Thread serviceThread;
-    private Vibrator vibrator;
-    private Date nextAlarmDate;
-    private long alarmDuration;
-    private long alarmRepeatDuration;
+public class RepeatService extends Service {
+  public static final String ACTION_PROPERTY = "RepeatService.action";
+  public static final String AUDIO_RESID_PROPERTY = "RepeatService.audio";
+  public static final String REPEAT_RATE_PROPERTY = "RepeatService.repeat.rate";
+  public static final String REPEAT_RATE_UNIT_PROPERTY = "RepeatService.repeat.rate.unit";
+  public static final String REPEAT_DURATION_PROPERTY = "RepeatService.repeat.duration";
+  public static final String REPEAT_DURATION_UNIT_PROPERTY = "RepeatService.duration.unit";
+  public static final String REST_DURATION_PROPERTY = "RepeatService.rest.duration";
+  public static final String REST_DURATION_UNIT_PROPERTY = "RepeatService.rest.duration.unit";
 
-    public RepeatService() {
+  private Runnable action;
+  private long repeatDuration;
+  private TimeUnit repeatDurationUnit;
+  private long repeatRate;
+  private TimeUnit repeatRateUnit;
+  private long restDuration;
+  private TimeUnit restDurationUnit;
+//  private Vibrator vibrator;
+  private ScheduledExecutorService scheduler;
+  private ScheduledFuture<?> repeatFuture;
+
+  @Override
+  public void onCreate() {
+    //TODO place vibrator to action
+//    vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+    scheduler = Executors.newScheduledThreadPool(1);
+  }
+
+  @Override
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    if(!parseParameters(intent)){
+      return START_NOT_STICKY;
     }
-
-    @Override
-    public void onCreate() {
-        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
-        SharedPreferences preferences = getSharedPreferences(MainActivity.PREFERENCES_NAME, Context.MODE_PRIVATE);
-        alarmDuration = preferences.getInt(getString(R.string.alarmDuration), getResources().getInteger(R.integer.defaultAlarmDuration));
-        alarmRepeatDuration = preferences.getInt(getString(R.string.alarmRepeatDuration), getResources().getInteger(R.integer.defaultAlarmRepeatDuration));
-        refreshAlarmDate();
-    }
-
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        if(serviceThread != null) {
-            serviceThread.interrupt();
+    long totalDuration = repeatDurationUnit.toMillis(repeatDuration) + restDurationUnit.toMillis(restDuration);
+    //repeatedly schedule action repeats
+    scheduler.scheduleAtFixedRate(new Runnable() {
+      @Override
+      public void run() {
+        repeatFuture = scheduler.scheduleAtFixedRate(action, 0, repeatRate, repeatRateUnit);
+      }
+    }, 0, totalDuration, TimeUnit.MILLISECONDS);
+    //in rest time cancel action repetition
+    scheduler.scheduleAtFixedRate(new Runnable() {
+      @Override
+      public void run() {
+        if(repeatFuture!= null && !repeatFuture.isCancelled()) {
+          repeatFuture.cancel(true);
         }
-        serviceThread = new Thread(this);
-        serviceThread.start();
-        return START_STICKY;
+      }
+    }, repeatDurationUnit.toMillis(repeatDuration), totalDuration, TimeUnit.MILLISECONDS );
+    return START_NOT_STICKY;
+  }
+
+  @Override
+  public void onDestroy() {
+    scheduler.shutdownNow();
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    // no binding to service
+    return null;
+  }
+
+  private boolean parseParameters(Intent intent) {
+    Bundle b = intent.getExtras();
+    if(!b.keySet().containsAll(Arrays.asList(
+            AUDIO_RESID_PROPERTY,
+            REPEAT_DURATION_PROPERTY,
+            REPEAT_DURATION_UNIT_PROPERTY,
+            REPEAT_RATE_PROPERTY,
+            REPEAT_RATE_UNIT_PROPERTY,
+            REST_DURATION_PROPERTY,
+            REST_DURATION_UNIT_PROPERTY))){
+      return false;
     }
+    final int resId = b.getInt(AUDIO_RESID_PROPERTY);
+    //TODO create subclass?
+    //TODO include vibration
+    action = new Runnable() {
+      @Override
+      public void run() {
+        final MediaPlayer player = MediaPlayer.create(RepeatService.this, resId);
+        player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+          @Override
+          public void onCompletion(MediaPlayer mp) {
+            player.release();
+          }
+        });
+        player.start();
+      }
+    };
+    repeatDuration = b.getLong(REPEAT_DURATION_PROPERTY);
+    repeatDurationUnit = TimeUnit.valueOf(b.get(REPEAT_DURATION_UNIT_PROPERTY).toString());
+    repeatRate = b.getLong(REPEAT_RATE_PROPERTY);
+    repeatRateUnit = TimeUnit.valueOf(b.get(REPEAT_RATE_UNIT_PROPERTY).toString());
+    restDuration = b.getLong(REST_DURATION_PROPERTY);
+    restDurationUnit = TimeUnit.valueOf(b.get(REST_DURATION_UNIT_PROPERTY).toString());
 
-    @Override
-    public void onDestroy() {
-        if(serviceThread != null) {
-            serviceThread.interrupt();
-        }
-    }
-
-    @Override
-    public IBinder onBind(Intent intent) {
-        // no binding to service
-        return null;
-    }
-
-    private void refreshAlarmDate() {
-        nextAlarmDate = new Date();
-        nextAlarmDate.setTime(nextAlarmDate.getTime() + alarmRepeatDuration * 1000L);
-    }
-
-    @Override
-    public void run() {
-        try {
-            while(true) {
-                long diff = nextAlarmDate.getTime() - System.currentTimeMillis();
-                if (diff > 0) {
-
-                    Thread.sleep(diff);
-
-                }
-                vibrator.vibrate(alarmDuration);
-                refreshAlarmDate();
-            }
-        } catch (InterruptedException e) {
-            // TODO handle exception
-        }
-    }
+    return true;
+  }
 }
